86d6ae32b205784bd4d8aa83ad6ad7c29a6ec263
CMathieu <couillardmathieu084@gmail.com>
2026-01-14 15:27:25 +0100

    CI addition

--- addition.test.js
+++ addition.test.js
@@ -1,1 +1,6 @@
-calculerAddition(15,5)
+const { default: expect } = require('expect');
+const { calculerAddition, appliquerReduction } = require('./addition');
+
+test('plat à 15€ + boisson à 5€ = 20€', () => {
+  expect(calculerAddition(15, 5)).toBe(20);
+});
